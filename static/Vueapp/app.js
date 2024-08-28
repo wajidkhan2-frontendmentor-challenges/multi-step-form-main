753797a27c331342b97a2bea74a3b4f63e1b3ee5
wajidkhan2 <khanw5310@gmail.com>
2024-08-29 00:33:38 +0500

    saving before going back to new method

--- static/Vueapp/app.js
+++ static/Vueapp/app.js
@@ -11,30 +11,74 @@ createApp({
             // },
             current_step: "step-1",
 
-            previous_active_link: ""
+            form_fields: {
+                step_1: { 
+                    user_name: '',
+                    user_name_error: 'false',
+
+                    email_eddress: '',
+                    email_eddress_error: 'false',
+
+                    phone_number: '',
+                    phone_number_error: 'false',
+                }
+            }
         }
     },
     // created() {
         
     // },
     methods: {
-        Change_step(event) {
-            
-            let tag = event.target.closest("a.link")
-            console.log(tag)
-            if (this.previous_active_link != "")
+        switch_stp_1() {
+            this.$refs.stp_1_lnk.dataset.active = "true";
+            this.$refs.stp_2_lnk.dataset.active = "false";
+            this.$refs.stp_3_lnk.dataset.active = "false";
+            this.$refs.stp_4_lnk.dataset.active = "false";
+
+            this.current_step = "step-1";
+        },
+        switch_stp_2() {
+            stp_1_good = this.check_step_1()
+            if(stp_1_good == true)
             {
-                this.previous_active_link.dataset.active = false;
+                this.$refs.stp_1_lnk.dataset.active = "false";
+                this.$refs.stp_2_lnk.dataset.active = "true";
+                this.$refs.stp_3_lnk.dataset.active = "false";
+                this.$refs.stp_4_lnk.dataset.active = "false";
+    
+                this.current_step = "step-2";
             }
-            tag.dataset.active = true;
-            this.current_step = tag.hash.slice(1);
-            this.previous_active_link = tag;
+        },
+        switch_stp_3() {
+            this.$refs.stp_1_lnk.dataset.active = "false";
+            this.$refs.stp_2_lnk.dataset.active = "false";
+            this.$refs.stp_3_lnk.dataset.active = "true";
+            this.$refs.stp_4_lnk.dataset.active = "false";
+
+            this.current_step = "step-3";
+        },
+        switch_stp_4() {
+            this.$refs.stp_1_lnk.dataset.active = "false";
+            this.$refs.stp_2_lnk.dataset.active = "false";
+            this.$refs.stp_3_lnk.dataset.active = "false";
+            this.$refs.stp_4_lnk.dataset.active = "true";
+
+            this.current_step = "step-4";
+        },
+
+        check_step_1() {
+
+            this.form_fields.step_1.user_name_error = "false";
+            if (this.form_fields.step_1.user_name == '')
+            {
+                this.form_fields.step_1.user_name_error = "true";
+            }
+            return false;
         }
     },
+
     mounted() {
-        let links = document.querySelectorAll("a.link");
-        console.log(links);
-        this.previous_active_link = links[0]
+
     }
 
 }).mount('#app')
